12ce5e3b6b35be59cdbe5ee4e2c920c7e63e7929
Sachin Saraswat <sswt.git@gmail.com>
2022-01-09 15:12:23 -0500

    Property List

--- basic/simplewebapp/src/main/java/com/example/simplewebapp/SimplewebappApplication.java
+++ basic/simplewebapp/src/main/java/com/example/simplewebapp/SimplewebappApplication.java
@@ -2,12 +2,14 @@ package com.example.simplewebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
 @ComponentScan
+@EnableConfigurationProperties
 public class SimplewebappApplication {
 
 	public static void main(String[] args) {
