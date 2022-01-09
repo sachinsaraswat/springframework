0f7ddcfc04de3d70934b6831f593d80bbd05299d
sachinsaraswat <sswt.git@gmail.com>
2022-01-09 16:12:40 -0500

    With random quotes

--- basic/simplewebapp/src/main/java/com/example/simplewebapp/SimplewebappApplication.java
+++ basic/simplewebapp/src/main/java/com/example/simplewebapp/SimplewebappApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableWebMvc
 @ComponentScan
 @EnableConfigurationProperties
+@PropertySource("classpath:application.yml")
 public class SimplewebappApplication {
 
 	public static void main(String[] args) {
--- basic/simplewebapp/src/main/java/com/example/simplewebapp/service/QuotesService.java
+++ basic/simplewebapp/src/main/java/com/example/simplewebapp/service/QuotesService.java
@@ -9,14 +9,26 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Random;
 
 @Service
 @Configuration
+@ConfigurationProperties
 public class QuotesService {
 
-    @Autowired
-    public List<String> quoteList;
+     @Value("${quoteList}")
+     public List<String> quoteList;
 
+
+    public List<String> getQuoteList() {
+        return quoteList;
+    }
+
+    public void setQuoteList(List<String> quoteList) {
+        this.quoteList = quoteList;
+    }
 
     @PostConstruct
     public  void initValue()
@@ -26,7 +38,14 @@ public class QuotesService {
     }
 
     public String getRandomQuote() {
-        return "Good morning , it is a wonderful day!!";
+       // quoteList.forEach((k)->System.out.println(k));
+
+        System.out.println("Quote Length::" + quoteList.size());
+        Random random = new Random();
+
+        int indx = random.nextInt(quoteList.size() -1) ;
+
+        return quoteList.get(indx).toString();
     }
 
     
