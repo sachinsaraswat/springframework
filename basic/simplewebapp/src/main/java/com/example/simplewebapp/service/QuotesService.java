de56300b64c93eb851bc14131246d26c71156114
Sachin Saraswat <sswt.git@gmail.com>
2022-01-09 13:24:07 -0500

    QuoteService

--- basic/simplewebapp/src/main/java/com/example/simplewebapp/service/QuotesService.java
+++ basic/simplewebapp/src/main/java/com/example/simplewebapp/service/QuotesService.java
@@ -1,6 +1,13 @@
 package com.example.simplewebapp.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class QuotesService {
+
+    public String getRandomQuote() {
+        return "Good morning , it is a wonderful day!!";
+    }
 
     
 }
