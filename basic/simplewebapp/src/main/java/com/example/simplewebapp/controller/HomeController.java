70e3d7484d933107236802011d35cf9c6c773b01
sachinsaraswat <sswt.git@gmail.com>
2022-01-30 10:56:42 -0500

    Added Quotes

--- basic/simplewebapp/src/main/java/com/example/simplewebapp/controller/HomeController.java
+++ basic/simplewebapp/src/main/java/com/example/simplewebapp/controller/HomeController.java
@@ -6,11 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    
 
-    @GetMapping({"/", "/home"})
-    public String home( Model model)
-    {
+    @GetMapping({ "/", "/home" })
+    public String home(Model model) {
         model.addAttribute("welcomeMessage", "Welcome To Simple Quotes Web app");
 
         return "index.html";
